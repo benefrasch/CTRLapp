337ea7599ae01d21df8bc3751db13b9796a4c417
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 21:58:48 +0200

    initial commit

---
+++ CTRLapp/CTRLapp/ValueConverters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CTRLapp
+{
+    public class HexToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Color.FromHex((string)value);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((Color)value).ToHex();
+        }
+    }
+    public class FloatToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)(double)value;
+        }
+    }
+    public class IntToFloatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)(double)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (string)value;
+        }
+    }
+}
