f32fb5c5b33b2e7aeaf6f267f7015e9fe0988018
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 15:44:04 +0200

    added settings menus

---
+++ CTRLapp/CTRLapp/Views/Settings_pages/GUI/edit_layouts/Joystick.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CTRLapp.Views.Settings_pages.GUI.edit_layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Joystick : ContentView
+    {
+        public Joystick()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler UpdateEvent;
+
+        public void UpdatePreview(Object sender, EventArgs e)
+        {
+            UpdateEvent.Invoke(null, null);
+        }
+    }
+}
---
+++ CTRLapp/CTRLapp/Views/Settings_pages/GUI/edit_layouts/Slider.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CTRLapp.Views.Settings_pages.GUI.edit_layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Slider : ContentView
+    {
+        public Slider()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler UpdateEvent;
+
+        public void UpdatePreview(Object sender, EventArgs e)
+        {
+            UpdateEvent.Invoke(null, null);
+        }
+    }
+}
