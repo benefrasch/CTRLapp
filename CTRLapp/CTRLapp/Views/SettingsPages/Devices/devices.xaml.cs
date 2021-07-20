2db14fd746b2e18e697ac81d50f77e5262e02ed9
BuildTools <unconfigured@null.spigotmc.org>
2021-07-20 22:30:38 +0200

    bluetooth connecting for paired devices

--- CTRLapp/CTRLapp/Views/SettingsPages/Devices/devices.xaml.cs
+++ CTRLapp/CTRLapp/Views/SettingsPages/Devices/devices.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
-using Plugin.BluetoothLE;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Exceptions;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,10 +11,55 @@ namespace CTRLapp.Views.SettingsPages.Devices
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Devices : ContentView
     {
+        public List<Plugin.BLE.Abstractions.Contracts.IDevice> deviceList = new List<Plugin.BLE.Abstractions.Contracts.IDevice>();
+        private Plugin.BLE.Abstractions.Contracts.IBluetoothLE ble;
+        private Plugin.BLE.Abstractions.Contracts.IAdapter adapter;
+
         public Devices()
         {
             InitializeComponent();
 
+            ble = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+
+            adapter.DeviceDiscovered += (s, a) =>
+            {
+                deviceList.Add(a.Device);
+                Debug.WriteLine("Device found" + a.Device.Name);
+            };
+
+            ble.StateChanged += (s, e) =>
+            {
+                Debug.WriteLine($"The bluetooth state changed to {e.NewState}");
+            };
+
+            deviceListView.BeginRefresh();
+        }
+
+        private async void DeviceListView_ItemSelected(object _, SelectedItemChangedEventArgs e)
+        {
+            
+            try
+            {
+                await adapter.ConnectToDeviceAsync((Plugin.BLE.Abstractions.Contracts.IDevice)e.SelectedItem);
+            }
+            catch (DeviceConnectionException m)
+            {
+                Debug.WriteLine(m.Message);
+            }
+        }
+
+        private async void RefreshDeviceList(object _ = null, System.EventArgs e = null)
+        {
+            Debug.WriteLine(ble.State);
+
+            var systemDevices = adapter.GetSystemConnectedOrPairedDevices();
+            deviceList.AddRange((List<Plugin.BLE.Abstractions.Contracts.IDevice>)systemDevices);
+
+            adapter.ScanTimeout = 10000;
+            await adapter.StartScanningForDevicesAsync();
+            deviceListView.EndRefresh();
+            deviceListView.ItemsSource = deviceList;
         }
     }
 }
